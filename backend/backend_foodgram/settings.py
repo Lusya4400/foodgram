50494302a484b8b2133724105d75415a60c11d62
Liudmila Usacheva <luda-a@rambler.ru>
2025-01-24 22:05:20 +0300

    version 4_14

--- backend/backend_foodgram/settings.py
+++ backend/backend_foodgram/settings.py
@@ -105,6 +105,7 @@ AUTH_USER_MODEL = 'recipes.User'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
